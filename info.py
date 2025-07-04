6ea7edc3b0a39b5afe0483ead0e286581fd9b37e
nahid270 <110238003+nahid270@users.noreply.github.com>
2025-07-04 18:20:58 +0600

    Add files via upload

---
+++ info.py
@@ -0,0 +1,40 @@
+import re
+from os import environ
+
+# Bot Session Name
+SESSION = environ.get('SESSION', 'TechVJBot')
+
+# Your Telegram Account Api Id And Api Hash
+API_ID = int(environ.get('API_ID', '22697010'))
+API_HASH = environ.get('API_HASH', 'fd88d7339b0371eb2a9501d523f3e2a7')
+
+# Bot Token, This Is Main Bot
+BOT_TOKEN = environ.get('BOT_TOKEN', "")
+
+# Admin Telegram Account Id For Withdraw Notification Or Anything Else
+ADMIN = int(environ.get('ADMIN', '8172129114'))
+
+# Back Up Bot Token For Fetching Message When Floodwait Comes
+BACKUP_BOT_TOKEN = environ.get('BACKUP_BOT_TOKEN', "")
+
+# Log Channel, In This Channel Your All File Stored.
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002869675678'))
+
+# Mongodb Database For User Link Click Count Etc Data Store.
+MONGODB_URI = environ.get("MONGODB_URI", "")
+
+# Stream Url Means Your Deploy Server App Url, Here You Media Will Be Stream And Ads Will Be Shown.
+STREAM_URL = environ.get("STREAM_URL", "")
+
+# This Link Used As Permanent Link That If Your Deploy App Deleted Then You Change Stream Url, So This Link Will Redirect To Stream Url.
+LINK_URL = environ.get("LINK_URL", "https://ctgmovies247.blogspot.com/p/ctg-movie.html")
+
+# Others, Not Usefull
+PORT = environ.get("PORT", "8080")
+MULTI_CLIENT = False
+SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
+PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
+if 'DYNO' in environ:
+    ON_HEROKU = True
+else:
+    ON_HEROKU = False
